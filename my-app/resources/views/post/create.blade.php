6c35b7ffdeab6d5abe972bda59864e5b3aad3c02
ysmk <0620komako@gmail.com>
2025-06-01 11:14:18 +0900

    feat: create view file for form

---
+++ my-app/resources/views/post/create.blade.php
@@ -0,0 +1,27 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            フォーム
+        </h2>
+    </x-slot>
+    <div class="max-w-7xl mx-auto px-6">
+        <form>
+            <div class="mt-8">
+                <div class="w-full flex flex-col">
+                    <label for="title" class="font-semibold mt-4">件名</label>
+                    <input type="text" name="title" class="w-auto py-2 border border-gray-300 round-md" id="title">
+                </div>
+            </div>
+
+            <div class="w-full flex flex-col">
+                <label for="body" class="font-semibold mt-4">本文</label>
+                <textarea name="body" class="w-auto py-2 border border-gray-300 round-md" id="body" cols="30" rows="5">
+                </textarea>
+            </div>
+
+            <x-primary-button class="mt-4">
+                送信する
+            </x-primary>
+        </form>
+    </div>
+</x-app-layout>
