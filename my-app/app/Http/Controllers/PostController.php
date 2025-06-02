9b6ff706a9b422b35ba0ca2deec6165a29c546ed
ysmk <0620komako@gmail.com>
2025-06-02 23:19:08 +0900

    feat: Implement post data storage in PostController

--- my-app/app/Http/Controllers/PostController.php
+++ my-app/app/Http/Controllers/PostController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     public function create(){
         return view('post.create');
+    }
+
+    public function store(Request $request){
+        $post =Post::create([
+            'title'=>$request->title,
+            'body'=>$request->body
+        ]);
+        return back();
     }
 }
