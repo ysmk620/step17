dfef7c6f2c590d9cda4e85a4e320681d3c45cb5e
ysmk <0620komako@gmail.com>
2025-06-08 07:23:00 +0900

    feat: Add where condition to filter post data

--- my-app/app/Http/Controllers/PostController.php
+++ my-app/app/Http/Controllers/PostController.php
@@ -25,7 +25,7 @@ class PostController extends Controller
     }
 
     public function index(){
-        $posts = Post::all();
+        $posts = Post::where('user_id', auth()->id())->get();
         return view('post.index',compact('posts'));
     }
 
