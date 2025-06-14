615f7d5a7d6ce057ae4dbba8f80c8026f16944d5
ysmk <0620komako@gmail.com>
2025-06-14 10:08:06 +0900

    feat: Create flash message component for post submission

--- my-app/resources/views/post/create.blade.php
+++ my-app/resources/views/post/create.blade.php
@@ -5,11 +5,12 @@
         </h2>
     </x-slot>
     <div class="max-w-7xl mx-auto px-6">
-        @if(session('message'))
+        <!-- @if(session('message'))
             <div class="text-red-600 font-bold">
                 {{session('message')}}
             </div>
-        @endif
+        @endif -->
+        <x-message :message="session('message')"/>
         <form method="post" action="{{route('post.store')}}">
             @csrf
             <div class="mt-8">
